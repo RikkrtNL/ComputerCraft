16b015d9bf36e07b4edc4d511a8d1867982bdd26
Rikkrt <152601119+RikkrtNL@users.noreply.github.com>
2024-05-13 20:23:13 +0200

    Delete Modem.lua

--- Modem.lua
+++
@@ -1,6 +0,0 @@
-local modem = peripheral.find("modem")
-modem.open(1)
-modem.transmit(2, 1, "data") --data = custom input
-while true do
-local event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
-print(message) end
